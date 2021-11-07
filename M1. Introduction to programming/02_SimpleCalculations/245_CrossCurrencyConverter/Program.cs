17c76cc21dc7c0383d67dce61ad53cc57c947de5
galet0 <galeto.miteva@gmail.com>
2021-11-07 23:26:06 +0200

    added Currency Converter and 1000Days

---
+++ M1. Introduction to programming/02_SimpleCalculations/245_CrossCurrencyConverter/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _245_CrossCurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double USD = 1.79549;
+            double EUR = 1.95583;
+            double GBP = 2.53405;
+
+            double amount = double.Parse(Console.ReadLine());
+            string inputCurrency = Console.ReadLine();
+            string outputCurrency = Console.ReadLine();
+
+            double result = 0;
+            if (inputCurrency.Equals("USD"))
+            {
+                result = amount * USD;
+            }
+            else if (inputCurrency.Equals("EUR"))
+            {
+                result = amount * EUR;
+            }
+            else if (inputCurrency.Equals("GBP"))
+            {
+                result = amount * GBP;
+            }
+            else if (inputCurrency.Equals("BGN"))
+            {
+                result = amount;
+            }
+
+            if (outputCurrency.Equals("USD"))
+            {
+                result = result / USD;
+            }
+            else if (outputCurrency.Equals("EUR"))
+            {
+                result = result / EUR;
+            }
+            else if (outputCurrency.Equals("GBP"))
+            {
+                result = result / GBP;
+            }
+
+            Console.WriteLine($"{result:f2} {outputCurrency}");
+        }
+    }
+}
---
+++ M1. Introduction to programming/02_SimpleCalculations/246_1000DaysOnTheEarth/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace _246_1000DaysOnTheEarth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string date = Console.ReadLine();
+                string format = "dd-MM-yyyy";
+                CultureInfo provider = CultureInfo.InvariantCulture;
+
+                DateTime dateTime = DateTime.ParseExact(date, format, provider).AddDays(999);
+                Console.WriteLine(dateTime.ToString("dd-MM-yyyy"));
+            }
+        }
+    }
+}
