9ffdea3d2f925ffed8554b4b8657c59bd35ccabe
galet0 <galeto.miteva@gmail.com>
2021-11-13 10:44:39 +0200

    Money and DailyProfit tasks added

---
+++ M1. Introduction to programming/02_SimpleCalculations/2410_Money/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2410_Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int btc = int.Parse(Console.ReadLine());
+            double yuan = double.Parse(Console.ReadLine());
+            double commissionInPercent = double.Parse(Console.ReadLine());
+
+            double result = ((btc * 1168) + (yuan * 0.15 * 1.76)) / 1.95;//euro
+            double commission = result * commissionInPercent / 100;
+
+            result = result - commission;
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
---
+++ M1. Introduction to programming/02_SimpleCalculations/2411_DailyProfit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2411_DailyProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double moneyPerDay = double.Parse(Console.ReadLine());
+            double dollarCourse = double.Parse(Console.ReadLine());
+
+            double monthlyProfit = days * moneyPerDay;//monthly
+            double yearlyProfit = (monthlyProfit * 12) + monthlyProfit * 2.5; //yearly
+
+            double vat = yearlyProfit * 0.25;
+            double clearProfit = (yearlyProfit - vat) * dollarCourse;
+            double result = clearProfit / 365;
+
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
