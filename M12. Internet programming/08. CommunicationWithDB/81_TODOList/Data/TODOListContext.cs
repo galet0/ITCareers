7ccf13d3b56b234aba409d66e3f706b4a1d94d5f
galet0 <galeto.miteva@gmail.com>
2021-11-19 15:19:53 +0200

    ToDo list app with scaffolded model with Controllers, Views and some records in DB

---
+++ M12. Internet programming/08. CommunicationWithDB/81_TODOList/Data/TODOListContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _81_TODOList.Models;
+
+namespace _81_TODOList.Data
+{
+    public class TODOListContext : DbContext
+    {
+        public TODOListContext (DbContextOptions<TODOListContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<_81_TODOList.Models.Task> Task { get; set; }
+    }
+}
---
+++ M12. Internet programming/08. CommunicationWithDB/81_TODOList/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _81_TODOList.Models
+{
+    public enum Status
+    {
+        [Display(Name = "Open")]
+        Open,
+
+        [Display(Name = "In Progress")]
+        InProgress,
+
+        [Display(Name = "Finished")]
+        Finished
+    }
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Status Status { get; set; }
+    }
+}
