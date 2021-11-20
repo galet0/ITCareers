334f9e74f1941ec6bcc45ab5042d3dc8628b4b28
galet0 <galeto.miteva@gmail.com>
2021-11-20 22:56:04 +0200

    to do list

---
+++ M12. Internet programming/08. CommunicationWithDB/ToDoList/81_ToDoList/Data/ToDoListContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _81_ToDoList.Models;
+
+namespace _81_ToDoList.Data
+{
+    public class ToDoListContext : DbContext
+    {
+        public ToDoListContext (DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<_81_ToDoList.Models.Task> Task { get; set; }
+    }
+}
---
+++ M12. Internet programming/08. CommunicationWithDB/ToDoList/81_ToDoList/Migrations/20211120205056_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _81_ToDoList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Task",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Task", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Task");
+        }
+    }
+}
